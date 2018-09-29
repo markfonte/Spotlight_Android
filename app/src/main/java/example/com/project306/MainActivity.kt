cce9cfd31eb610c859566743112f2a6a5cd3e886
Mark Fonte <mark@fonte.com>
2018-09-28 22:07:56 -0400

    implemented basic navigation using Navigation Architecture Components

--- app/src/main/java/example/com/project306/MainActivity.kt
+++ app/src/main/java/example/com/project306/MainActivity.kt
@@ -2,6 +2,11 @@ package example.com.project306
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.design.widget.BottomNavigationView
+import androidx.navigation.NavController
+import androidx.navigation.NavHost
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
 import example.com.project306.ui.main.MainFragment
 
 class MainActivity : AppCompatActivity() {
@@ -9,10 +14,17 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.main_activity)
-        if (savedInstanceState == null) {
-            supportFragmentManager.beginTransaction()
-                    .replace(R.id.container, MainFragment.newInstance())
-                    .commitNow()
+
+        val navHost: NavHostFragment = supportFragmentManager.findFragmentById(R.id.main_nav_host_fragment) as NavHostFragment? ?: return
+
+        val navController = navHost.navController
+
+        initializeBottomNav(navController)
+    }
+
+    private fun initializeBottomNav(navController: NavController) {
+        findViewById<BottomNavigationView>(R.id.bottom_nav)?.let { bottomNavigationView ->
+            NavigationUI.setupWithNavController(bottomNavigationView, navController)
         }
     }
 
