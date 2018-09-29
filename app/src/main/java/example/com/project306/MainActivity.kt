8e62c6b8d2661f31f62f25c10080b5a982da4985
Mark Fonte <mark@fonte.com>
2018-09-28 20:55:08 -0400

    init

---
+++ app/src/main/java/example/com/project306/MainActivity.kt
@@ -0,0 +1,19 @@
+package example.com.project306
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import example.com.project306.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                    .replace(R.id.container, MainFragment.newInstance())
+                    .commitNow()
+        }
+    }
+
+}
---
+++ app/src/main/java/example/com/project306/ui/main/MainViewModel.kt
@@ -0,0 +1,7 @@
+package example.com.project306.ui.main
+
+import android.arch.lifecycle.ViewModel
+
+class MainViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
