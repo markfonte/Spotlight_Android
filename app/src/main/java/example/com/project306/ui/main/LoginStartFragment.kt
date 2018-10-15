f4b114f2b2bba3012435e19ca9e044ce664c8d35
Mark Fonte <mark@fonte.com>
2018-10-14 23:55:30 -0400

    added start page

---
+++ app/src/main/java/example/com/project306/ui/main/LoginStartFragment.kt
@@ -0,0 +1,10 @@
+package example.com.project306.ui.main
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+
+class LoginStartFragment : Fragment() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
