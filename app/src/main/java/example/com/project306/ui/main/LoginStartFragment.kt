5c1c93a72ea125c81e211deedfbdaa7bfd0626d2
Mark Fonte <mark@fonte.com>
2018-10-15 23:53:30 -0400

    small tweak

--- app/src/main/java/example/com/project306/ui/main/LoginStartFragment.kt
+++ app/src/main/java/example/com/project306/ui/main/LoginStartFragment.kt
@@ -1,10 +1,17 @@
 package example.com.project306.ui.main
 
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 
 class LoginStartFragment : Fragment() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return super.onCreateView(inflater, container, savedInstanceState)
     }
 }
