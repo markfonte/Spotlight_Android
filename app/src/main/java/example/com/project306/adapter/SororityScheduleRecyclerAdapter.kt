8941e854d94a9514d846b5b55f5d2a630c55f754
Mark Fonte <mark@fonte.com>
2018-11-09 21:39:39 -0500

    improvements to choose values recycler view

--- app/src/main/java/example/com/project306/adapter/SororityScheduleRecyclerAdapter.kt
+++ app/src/main/java/example/com/project306/adapter/SororityScheduleRecyclerAdapter.kt
@@ -7,11 +7,12 @@ import android.widget.TextView
 import example.com.project306.R
 import example.com.project306.util.SororityTimeSlot
 
-class HomeFragmentRecyclerAdapter(private val sororityTimeSlots: MutableList<SororityTimeSlot>) : androidx.recyclerview.widget.RecyclerView.Adapter<HomeFragmentRecyclerAdapter.ViewHolder>() {
+class SororityScheduleRecyclerAdapter(private val sororityTimeSlots: MutableList<SororityTimeSlot>) : androidx.recyclerview.widget.RecyclerView.Adapter<SororityScheduleRecyclerAdapter.ViewHolder>() {
 
     class ViewHolder(v: View) : androidx.recyclerview.widget.RecyclerView.ViewHolder(v) {
-        val sororityName: TextView = v.findViewById(R.id.home_fragment_recycler_sorority_name)
-        val timeSlot: TextView = v.findViewById(R.id.home_fragment_recycler_time_slot)
+        val sororityName: TextView = v.findViewById(R.id.sorority_schedule_recycler_sorority_name)
+        val timeSlot: TextView = v.findViewById(R.id.sorority_schedule_recycler_time_slot)
+
     }
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         return ViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.main_fragment_recycler_row, parent, false))
@@ -20,9 +21,10 @@ class HomeFragmentRecyclerAdapter(private val sororityTimeSlots: MutableList<Sor
     override fun getItemCount(): Int = sororityTimeSlots.size
 
     override fun onBindViewHolder(viewHolder: ViewHolder, position: Int) {
-        viewHolder.sororityName.text = sororityTimeSlots[position].SororityName
-        val temp : String = sororityTimeSlots[position].StartTime + " : " + sororityTimeSlots[position].EndTime
+        viewHolder.sororityName.text = sororityTimeSlots[position].HouseDisplayName
+        val temp : String = sororityTimeSlots[position].Time!!
         viewHolder.timeSlot.text = temp
+
     }
 
 }
--- app/src/main/java/example/com/project306/util/ObjectUtils.kt
+++ app/src/main/java/example/com/project306/util/ObjectUtils.kt
@@ -3,13 +3,14 @@ package example.com.project306.util
 import com.google.firebase.auth.FirebaseUser
 
 data class SororityTimeSlot(
-        val SororityName: String?,
-        val StartTime: String?,
-        val EndTime: String?
+        val Time: String?,
+        val HouseDisplayName: String?,
+        val HouseGreekLetters: String?,
+        val HouseStreetAddress: String?
 )
 
 data class User(
         var FirebaseUser: FirebaseUser,
         var DisplayName: String?,
-        var isEmailVerified: Boolean
+        var IsEmailVerified: Boolean
 )
