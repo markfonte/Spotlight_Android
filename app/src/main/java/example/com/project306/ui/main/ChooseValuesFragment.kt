1b1340a276573eea1617d5ccaa54ab74eecaea91
Mark Fonte <mark@fonte.com>
2018-11-14 02:18:20 -0500

    confirmation pop-ups work. just need to add firebase service implementation

--- app/src/main/java/example/com/project306/ui/main/ChooseValuesFragment.kt
+++ app/src/main/java/example/com/project306/ui/main/ChooseValuesFragment.kt
@@ -1,5 +1,6 @@
 package example.com.project306.ui.main
 
+import android.app.AlertDialog
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -14,10 +15,20 @@ import example.com.project306.R
 import example.com.project306.databinding.FragmentChooseValuesBinding
 import example.com.project306.util.InjectorUtils
 import kotlinx.android.synthetic.main.fragment_choose_values.*
+import java.util.*
+import kotlin.collections.HashMap
+
 
 class ChooseValuesFragment : Fragment() {
     private lateinit var chooseValuesFragmentViewModel: ChooseValuesViewModel
     private lateinit var panhelValues: ArrayList<*>
+    private lateinit var currentlyCheckedBoxes: HashMap<Int, String>
+    private lateinit var submittedCheckboxes: Stack<String>
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        submittedCheckboxes = Stack()
+    }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val factory: ChooseValuesViewModelFactory = InjectorUtils.provideChooseValuesViewModelFactory()
@@ -33,8 +44,60 @@ class ChooseValuesFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
         chooseValuesFragmentViewModel.panhelValues.observe(this, Observer {
             panhelValues = it
+            currentlyCheckedBoxes = HashMap()
+            var i = 0
+            while (i < panhelValues.size) {
+                currentlyCheckedBoxes[i] = ""
+                ++i
+            }
             buildRadioButtons()
         })
+        choose_values_submit_button.setOnClickListener {
+            var count = 0
+            var i = 0
+            while (i < panhelValues.size) {
+                if (currentlyCheckedBoxes[i] != "") {
+                    ++count
+                    submittedCheckboxes.push(currentlyCheckedBoxes[i])
+                }
+                ++i
+            }
+            if (count == 3) {
+                sendConfirmationAlertDialog()
+            } else {
+                sendErrorAlertDialog()
+            }
+        }
+    }
+
+    private fun sendErrorAlertDialog() {
+        val alertDialog: AlertDialog = AlertDialog.Builder(context, android.R.style.Theme_Material_Dialog_Alert)
+                .setTitle("Invalid Submission")
+                .setMessage("Please select exactly 3 values")
+                .setPositiveButton(android.R.string.ok, null)
+                .setIcon(android.R.drawable.ic_dialog_alert)
+                .create()
+        alertDialog.setOnShowListener {
+            alertDialog.getButton(AlertDialog.BUTTON_POSITIVE).setOnClickListener {
+                alertDialog.dismiss()
+            }
+        }
+        alertDialog.show()
+    }
+
+    private fun sendConfirmationAlertDialog() {
+        val alertDialog: AlertDialog = AlertDialog.Builder(context, android.R.style.Theme_Material_Dialog_Alert)
+                .setTitle("Confirm Your Selections")
+                .setMessage("Select ${submittedCheckboxes.pop()}, ${submittedCheckboxes.pop()}, and ${submittedCheckboxes.pop()}?")
+                .setPositiveButton(android.R.string.yes, null)
+                .setNegativeButton(android.R.string.no, null)
+                .create()
+        alertDialog.setOnShowListener {
+            alertDialog.getButton(AlertDialog.BUTTON_POSITIVE).setOnClickListener {
+                //send to database, release if successful
+            }
+        }
+        alertDialog.show()
     }
 
     private fun buildRadioButtons() {
@@ -44,6 +107,13 @@ class ChooseValuesFragment : Fragment() {
             val newCheckbox = CheckBox(context)
             newCheckbox.id = counter
             newCheckbox.text = i.toString()
+            newCheckbox.setOnClickListener {
+                if ((it as CheckBox).isChecked) {
+                    currentlyCheckedBoxes[it.id] = it.text.toString()
+                } else {
+                    currentlyCheckedBoxes[it.id] = ""
+                }
+            }
             checkboxHolder.addView(newCheckbox)
         }
         choose_values_checkbox_holder.addView(checkboxHolder)
