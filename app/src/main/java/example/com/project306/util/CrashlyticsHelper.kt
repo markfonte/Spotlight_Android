80321aec230d1d0d52e94c4cc0931bfe218cb811
Mark Fonte <mark@fonte.com>
2019-02-14 02:48:46 -0500

    finished implementing crashlytics for firebase service

--- app/src/main/java/example/com/project306/util/CrashlyticsHelper.kt
+++ app/src/main/java/example/com/project306/util/CrashlyticsHelper.kt
@@ -6,12 +6,23 @@ import com.google.android.gms.tasks.Task
 
 class CrashlyticsHelper {
     companion object {
-        fun logErrorTask(task: Task<*>, logTag: String = "", functionName: String = "", message: String = "") {
-            Crashlytics.log(Log.ERROR, logTag, "FirebaseService.kt::$functionName: Error in task - Message: $message Result: ${task.result.toString()}; Exception: ${task.exception.toString()}.")
+
+        fun logDebug(logTag: String = "", functionName: String = "", message: String = "") {
+            Crashlytics.log(Log.ERROR, logTag, "$functionName: Error in task - Message: $message")
         }
 
         fun logDebugTask(task: Task<*>, logTag: String = "", functionName: String = "", message: String = "") {
-            Crashlytics.log(Log.DEBUG, logTag, "FirebaseService.kt::$functionName: Task result - Message: $message Result: ${task.result.toString()}; Exception(if any): ${task.exception.toString()}.")
+            Crashlytics.log(Log.DEBUG, logTag, "$functionName: Task result - Message: $message Result: ${task.result.toString()}; Exception(if any): ${task.exception.toString()}.")
+        }
+
+        fun logError(exception: Exception? = null, logTag: String = "", functionName: String = "", message: String = "") {
+            Crashlytics.log(Log.ERROR, logTag, "$functionName: Error in task - Message: $message Exception: ${exception.toString()}")
+            Crashlytics.logException(exception)
+        }
+
+        fun logErrorTask(task: Task<*>, logTag: String = "", functionName: String = "", message: String = "") {
+            Crashlytics.log(Log.ERROR, logTag, "$functionName: Error in task - Message: $message Result: ${task.result.toString()}; Exception: ${task.exception.toString()}.")
+            Crashlytics.logException(task.exception)
         }
     }
 }
